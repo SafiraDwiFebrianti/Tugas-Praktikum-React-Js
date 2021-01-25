1d5c02a64b72d394790c8fdae86635814b6feeaf
SafiraDwiFebrianti <>
2021-01-25 09:47:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Bmi from './Component/Bmi';
+import Bank from './Component/Bank';
+import Harga from './Component/Harga';
+import Biner from './Component/Biner';
+import Oktal from './Component/Oktal';
+import Desimal from './Component/Desimal';
+import Heksa from './Component/Heksa';
+
+function App() {
+  return (
+    <div>
+      <Bmi />
+      <Bank />
+      <Harga />
+      <Biner />
+      <Oktal />
+      <Desimal />
+      <Heksa />
+    </div>
+  );
+}
+
+export default App;
